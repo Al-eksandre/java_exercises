b467acea107073e8d3f1c7b2bb82506122278c84
Al-eksandre <pitskhelauri.aleksandre@gmail.com>
2025-01-06 18:20:00 +0400

    Solution

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Add binary test");
+        System.out.println(addBinary("11", "1"));
+    }
+
+    public static String addBinary(String a, String b) {
+        int result = Integer.sum(Integer.parseInt(a, 2), Integer.parseInt(b, 2));
+        return Integer.toString(result, 2);
     }
 }
