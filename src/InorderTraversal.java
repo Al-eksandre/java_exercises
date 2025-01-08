1800c92effcd973871c37d9c2c46c6e3f68553e2
Al-eksandre <pitskhelauri.aleksandre@gmail.com>
2025-01-08 15:55:58 +0400

    Binary Tree Inorder Traversal Solution

---
+++ src/InorderTraversal.java
@@ -0,0 +1,19 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InorderTraversal {
+//    public static List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        dfs(result, root);
+//        return result;
+//    }
+//
+//    public static void dfs(List<Integer> result, TreeNode node) {
+//        if (node != null) {
+//            if (node.left != null) dfs(result, node.left);
+//            result.add(node.val);
+//            if (node.right != null) dfs(result, node.right);
+//        }
+//    }
+}
